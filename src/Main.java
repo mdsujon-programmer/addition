1d509cfd89de3336696e169c9c068c9d9c717f8e
mdsujon-programmer <mdsujonblog009@gmail.coomm>
2024-12-04 19:25:07 +0600

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inter number a: ");
+        int a = sc.nextInt();
+        System.out.println("Inter number b: ");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println("Sum of Two number: "+sum);
+
+    }
+}
